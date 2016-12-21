a91e6ceae59f1ecb558a38504ef3c5ba9edaf92b
Daniel Shorstein <dshorstein@gmail.com>
2016-12-21 15:08:52 +0000

    Tried adding some help and cancel features...

--- reddit_joke.py
+++ reddit_joke.py
@@ -17,7 +17,8 @@ logging.getLogger("flask_ask").setLevel(logging.DEBUG)
 @ask.launch
 def ask_joke_type():
     welcome = "Welcome! What kind of joke would you like to hear?"
-    return question(welcome)
+    return question(welcome).reprompt(welcome).simple_card('Jokebox', welcome)
+
 
 @ask.intent("JokeType")
 def tell_joke(joke_type):
@@ -27,44 +28,33 @@ def tell_joke(joke_type):
             jokehead, joke, joke_type = reddit_play.get_joke(joke_type)
             msg = "I couldn't understand you, so instead I'll tell you a joke about {}..... ".format(joke_type)
 
+        elif joke_type == 'help':
+            help()
+
+        elif joke_type in ('stop', 'cancel'):
+            msg = 'Later dude!'
+            return statement(msg).simple_card('Jokebox', msg)
+
         else:
             jokehead, joke, joke_type = reddit_play.get_joke(joke_type)
             msg = 'Ok, get ready for a joke about {}..... '.format(joke_type)
 
         msg = msg + jokehead + '...' + joke
-        return statement(msg)
+        return statement(msg).simple_card('Jokebox', msg)
     except Exception as e:
-        print('Oh no, something went wrong... {}'.format(e))
+        msg = "Unfortunately I don't feel very funny today. Please try again!"
+        return statement(msg).simple_card('Jokebox', msg)
 
-####EXPERIMENTAL SHIT####
-try:
-    from cah_randomizer import create_combo
 
-    app2 = Flask('cah_search')
-
-    ask2 = Ask(app2, "/cah_randomizer")
-
-    @ask2.intent("YesIntent")
-    def welcome():
-        welcome = "Are you ready for some fucked up shit?"
-        return question(welcome)
-
-    @ask2.intent("YesIntent")
-    def get_cards():
-
-        msg = create_combo()
-        return statement(msg)
-
-except:
-    print('SOMETHING FUCKED UP')
+@ask.intent('AMAZON.HelpIntent')
+def help():
+    speech_text = '''Jokebox is a hilarious app that will tell you a joke about
+        whatever you like. For example, you can ask Jokebox for a joke about
+        Russia, or you can ask for a pizza party joke.'''
+    return question(speech_text).reprompt(speech_text).simple_card('HelloWorld', speech_text)
 
 
 if __name__ == '__main__':
     # print(ask_joke_type().__dict__)
     # print(tell_joke('asshole').__dict__)
     app.run(debug=True)
-    try:
-        app2.run(debug=True)
-    except:
-        pass
-
