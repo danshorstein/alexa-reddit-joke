27453d5ed8f99a2183388f9b973d2cfb7763b738
Daniel Shorstein <dshorstein@gmail.com>
2016-12-21 15:31:48 +0000

    Fixed cancel, stop, and help. Added message boxes and reprompts.

--- reddit_joke.py
+++ reddit_joke.py
@@ -17,7 +17,7 @@ logging.getLogger("flask_ask").setLevel(logging.DEBUG)
 @ask.launch
 def ask_joke_type():
     welcome = "Welcome! What kind of joke would you like to hear?"
-    return question(welcome).reprompt(welcome).simple_card('Jokebox', welcome)
+    return question(welcome).reprompt('What kind of joke would you like?')
 
 
 @ask.intent("JokeType")
@@ -28,30 +28,43 @@ def tell_joke(joke_type):
             jokehead, joke, joke_type = reddit_play.get_joke(joke_type)
             msg = "I couldn't understand you, so instead I'll tell you a joke about {}..... ".format(joke_type)
 
-        elif joke_type == 'help':
-            help()
-
-        elif joke_type in ('stop', 'cancel'):
-            msg = 'Later dude!'
-            return statement(msg).simple_card('Jokebox', msg)
 
         else:
             jokehead, joke, joke_type = reddit_play.get_joke(joke_type)
-            msg = 'Ok, get ready for a joke about {}..... '.format(joke_type)
+            msg1 = 'Ok, get ready for a joke about {}..... '.format(joke_type)
+            msg2 = 'All right, here is one of my favorite {} jokes..... '.format(joke_type)
+            msg3 = 'Ready or not, here comes a jokea bout {}..... '.format(joke_type)
+            msg = random.choice([msg1, msg2, msg3])
 
         msg = msg + jokehead + '...' + joke
         return statement(msg).simple_card('Jokebox', msg)
-    except Exception as e:
-        msg = "Unfortunately I don't feel very funny today. Please try again!"
+
+    except:
+        msg = "Unfortunately I don't feel very funny today. Please try again later!"
         return statement(msg).simple_card('Jokebox', msg)
 
 
 @ask.intent('AMAZON.HelpIntent')
 def help():
-    speech_text = '''Jokebox is a hilarious app that will tell you a joke about
-        whatever you like. For example, you can ask Jokebox for a joke about
-        Russia, or you can ask for a pizza party joke.'''
-    return question(speech_text).reprompt(speech_text).simple_card('HelloWorld', speech_text)
+    speech_text = 'Jokebox is an unofficial Reddit joke search app that ' +\
+      'will tell you a joke about whatever you like. ' +\
+      'For example, you can ask Jokebox for a joke about Russia, ' +\
+      'or you can ask for a pizza party joke. What kind of joke would ' +\
+      'you like to hear?'
+    reprompt_text = 'What kind of joke would you like?'
+    return question(speech_text).reprompt(reprompt_text)
+
+
+@ask.intent('AMAZON.StopIntent')
+def stop():
+    msg = 'Later dude!'
+    return statement(msg)
+
+
+@ask.intent('AMAZON.CancelIntent')
+def cancel():
+    msg = 'Later dude!'
+    return statement(msg)
 
 
 if __name__ == '__main__':
